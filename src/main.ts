8bc0221a1beadf1098831341af1843bd94de3f9d
Sahriar Nahin <sahriar.nahin@sazim.io>
2024-09-01 18:03:27 +0600

    cors and validation pipe setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe, VersioningType } from "@nestjs/common";
+import { getAllowedMethods, getCorsConfig } from "./common/config/cors.config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: "1",
+    prefix: "api/v",
+  });
+  app.enableCors({
+    methods: getAllowedMethods(),
+    ...getCorsConfig(),
+  });
 }
 bootstrap();
